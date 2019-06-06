f7b44bc48f9a2a14dbbd2da4a02125eadab2a9d5
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-06 05:59:23 -0700

    Complete Scaffolding
    Complete Scaffolding with Semantic UI support and Clean src folder with index.js

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonsDisplay = (props) => {
+    return (
+        <div>
+            Seasons Display!
+        </div>
+    );
+};
+
+export default SeasonsDisplay;
