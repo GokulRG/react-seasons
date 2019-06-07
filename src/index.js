f9a237d04f179e58a3b0af6f81d76f8dbbecf112
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-06 20:14:40 -0700

    Adding State Support to React Components

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        //This is the only time when you will directly manipulate the 
+        //state object without calling setState.
+        this.state = { lat : null , long : null};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
+            (position) => {
+                this.setState({lat : position.coords.latitude, long : position.coords.longitude})
+            },
+            (error) => {
+                console.log(error);
+            }
         );
+    }
+
+    render() {
+        
+        //Do not use heavy API calls or any other methods here
+        //because the render method will be repeatedly called
+        //throughout the lifecycle of the component.
 
         return (
-            <div>Latitude : </div>
+            <div>
+                <span>Latitude :  {this.state.lat}</span> <br/>
+                <span>Longitude : {this.state.long}</span>
+            </div>
         );
     }
 }
