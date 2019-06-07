f83977ae7574fd60f042f2fcc2c1c3ca2ce46913
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-06 21:19:26 -0700

    Adding Conditional Rendering
    Conditionally Rendering components based on the results retrieved. Error alone displayed in case of error and results are displayed in case of success

--- src/index.js
+++ src/index.js
@@ -8,34 +8,48 @@ class App extends React.Component {
 
         //This is the only time when you will directly manipulate the 
         //state object without calling setState.
-        this.state = { lat : null , long : null, errorMessage: ''};
+        this.state = { lat: null, long: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState({lat : position.coords.latitude, long : position.coords.longitude});
+                this.setState({ lat: position.coords.latitude, long: position.coords.longitude });
             },
             (error) => {
                 //Graceful error handling.
                 console.log(error);
-                this.setState({errorMessage: error.message});
+                this.setState({ errorMessage: error.message });
             }
         );
     }
 
     render() {
-        
+
         //Do not use heavy API calls or any other methods here
         //because the render method will be repeatedly called
         //throughout the lifecycle of the component.
 
-        return (
-            <div>
-                <span>Latitude :  {this.state.lat}</span> <br/>
-                <span>Longitude : {this.state.long}</span> <br/>
-                <span>Error : {this.state.errorMessage}</span>
-            </div>
-        );
+        //Adding Conditional Rendering
+        // Render only location info when available
+        //Else only error if available
+        if (this.state.lat && this.state.long && !this.state.errorMessage) {
+            return (
+                <div>
+                    <span>Latitude :  {this.state.lat}</span> <br />
+                    <span>Longitude : {this.state.long}</span> <br />  
+                </div>
+            );
+        }
+
+        if (!this.state.lat && !this.state.long && this.state.errorMessage) {
+            return (
+                <div>
+                    <span> Error : <span style={{color:'red'}}>{this.state.errorMessage}</span></span>
+                </div>
+            );
+        }
+
+        return <span> Loading .....</span>
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
