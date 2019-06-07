e1241ffb06255a5bc868ec59b7b7faaa9a5663fc
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-06 21:13:27 -0700

    Error Handling Changes
    Adding Graceful Error Handling to the Application

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ class App extends React.Component {
 
         //This is the only time when you will directly manipulate the 
         //state object without calling setState.
-        this.state = { lat : null , long : null};
+        this.state = { lat : null , long : null, errorMessage: ''};
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState({lat : position.coords.latitude, long : position.coords.longitude})
+                this.setState({lat : position.coords.latitude, long : position.coords.longitude});
             },
             (error) => {
+                //Graceful error handling.
                 console.log(error);
+                this.setState({errorMessage: error.message});
             }
         );
     }
@@ -29,7 +31,8 @@ class App extends React.Component {
         return (
             <div>
                 <span>Latitude :  {this.state.lat}</span> <br/>
-                <span>Longitude : {this.state.long}</span>
+                <span>Longitude : {this.state.long}</span> <br/>
+                <span>Error : {this.state.errorMessage}</span>
             </div>
         );
     }
