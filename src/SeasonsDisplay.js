e8fd24ca39401a70811753bea6905a48c4938e42
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-09 17:31:03 -0700

    Adding Icons and Config Object for Styling
    Adding icons, moving styling and other minutia to the config object, removing cluttered ternary expressions.

--- src/SeasonsDisplay.js
+++ src/SeasonsDisplay.js
@@ -11,14 +11,36 @@ const getSeason = (lat, month) => {
 
 }
 
+const config = {
+    summer : {
+        iconName : 'sun icon',
+        size : 'massive',
+        color : 'yellow',
+        displayMessage : 'Let\'s hit the beach!'
+    },
+    winter : {
+        iconName : 'snowflake icon',
+        size : 'massive',
+        color : 'teal',
+        displayMessage : 'Brrrrr.. It\'s cold out here!'
+    }
+};
+
 const SeasonsDisplay = (props) => {
 
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
+
+    //If we see repeated ternary operators like this and unnecessary clutter inside the method, it can
+    //be moved to a config object
+    /*const displayMessage = (season === 'winter') ? "" : ""
+    const iconString = (season === 'winter') ? "blue snowflake icon" : "yellow sun icon"; */
+    const {iconName, size, color, displayMessage} = config[season];
 
     return (
         <div>
-            {season}
+            <i className={`${iconName} ${size} ${color}`} /> <br/>
+            {displayMessage} <br/>
+            <i className={`${iconName} ${size} ${color}`} />
         </div>
     );
 };
